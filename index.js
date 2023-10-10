a986ae2ebe003ed6fe400fa92a2c3280c574a110
Ramos Raquel <raquelramosdasilva65@gmail.com>
2023-10-10 14:16:58 -0300

    recebendo parametros na rota

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ const app = express()
 
 const CaminhoBase = path.join(__dirname, "templates")
 
+app.get('/usuarios/:id', (requisicao, resposta) => {
+    const id = requisicao.params.id
+
+    console.log(`Acessando dados do usuario ${id}`)
+
+    resposta.sendFile(`${CaminhoBase}/usuarios.html`)
+})
 
 app.get('/', (requisicao, resposta) => {
-    // resposta.sendFile(`${CaminhoBase}/index.html`)
-    resposta.send("Teste do nodemon")
+    resposta.sendFile(`${CaminhoBase}/index.html`)
 })
 app.listen(3000,() => {
     console.log("Servidor rodando na porta 3000!")
